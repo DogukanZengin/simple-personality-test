6ef0c05fdbed4ff5add04565a1b6ef1c300b740e
dogukanzengin <dogukanzen@gmail.com>
2020-06-25 22:39:55 +0300

    - Dockerfile updates - Controller test classes added

--- backend/src/test/java/com/dz/io/controller/CategoryControllerSpec.java
+++ backend/src/test/java/com/dz/io/controller/CategoryControllerSpec.java
@@ -5,6 +5,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(AnswerController.class)
-public class AnswerControllerSpec {
+@WebMvcTest(CategoryController.class)
+public class CategoryControllerSpec {
 }
--- backend/src/test/java/com/dz/io/controller/QuestionControllerSpec.java
+++ backend/src/test/java/com/dz/io/controller/QuestionControllerSpec.java
@@ -1,4 +1,24 @@
 package com.dz.io.controller;
 
+import com.dz.io.service.QuestionService;
+import org.junit.runner.RunWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(QuestionController.class)
 public class QuestionControllerSpec {
+    @MockBean
+    private QuestionService questionService;
+
+    @MockBean
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private MockMvc mvc;
+
 }
